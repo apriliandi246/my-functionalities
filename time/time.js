973ec75202ea77bf4359bfe493c152c0c530dbea
parhan <apriliandi642@gmail.com>
2020-12-11 19:39:10 +0700

    add new feature and refactor code

--- time/time.js
+++ time/time.js
@@ -17,7 +17,6 @@ class Time {
       });
    }
 
-   // using number of day in javascript date method
    getNameOfDay(numberOfDay) {
       const days = [
          "Sunday",
@@ -31,7 +30,11 @@ class Time {
       return days[numberOfDay];
    }
 
-   // using number of month in javascript date method
+   getAbbreviationOfDay(numberOfDay) {
+      const days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+      return days[numberOfDay];
+   }
+
    getNameOfMonth(numberOfMonth) {
       const months = [
          "January",
@@ -50,76 +53,96 @@ class Time {
       return months[numberOfMonth];
    }
 
+   getAbbreviationOfMonth(numberOfMonth) {
+      const months = [
+         "Jan",
+         "Feb",
+         "Mar",
+         "Apr",
+         "May",
+         "Jun",
+         "Jul",
+         "Aug",
+         "Sept",
+         "Oct",
+         "Nov",
+         "Dec",
+      ];
+      return months[numberOfMonth];
+   }
+
+   getAbbreviationOfTime(time) {
+      const times = {
+         second: "sec",
+         minute: "min",
+         hour: "hr",
+      };
+      return times[time];
+   }
+
    /*
     * Levels :
     * hard => (Friday, April 6, 2020)
     * medium => (April 6, 2020)
     * easy => (April 2020)
     */
-   format(level) {
+   getDateFormat(level, format) {
+      if (["easy", "medium", "hard"].includes(level) === false) {
+         throw new Error("Level Not Found");
+      }
+
+      if (["normal", "short"].includes(format) === false) {
+         throw new Error("Format Not Found");
+      }
+
       let date = this.past.getDate();
       let year = this.past.getFullYear();
-      let day = this.getNameOfDay(this.past.getDay());
-      let month = this.getNameOfMonth(this.past.getMonth());
+      let day =
+         format === "normal"
+            ? this.getNameOfDay(this.past.getDay())
+            : this.getAbbreviationOfDay(this.past.getDay());
+      let month =
+         format === "normal"
+            ? this.getNameOfMonth(this.past.getMonth())
+            : this.getAbbreviationOfMonth(this.past.getMonth());
 
-      if (level === "easy") {
-         return `${month} ${year}`;
-      }
+      let formatDate = {
+         easy: `${month} ${year}`,
+         medium: `${month} ${date}, ${year}`,
+         hard: `${day}, ${month} ${date}, ${year}`,
+      };
 
-      if (level === "medium") {
-         return `${month} ${date}, ${year}`;
-      }
-
-      if (level === "hard") {
-         return `${day}, ${month} ${date}, ${year}`;
-      }
-
-      throw new Error("Level Not Found");
+      return formatDate[level];
    }
 
    fromNow() {
-      const difference = this.now / 1000 - this.past / 1000;
-      const hour = Math.floor(difference / 3600);
-      const diff = difference - hour * 3600;
-      const minute = Math.floor(diff / 60);
+      let difference = this.now / 1000 - this.past / 1000;
+      let hour = Math.floor(difference / 3600);
+      let diff = difference - hour * 3600;
+      let minute = Math.floor(diff / 60);
 
-      const day = Math.floor(hour / 24);
-      const week = Math.floor(day / 7);
-      const month = Math.floor(week / 4.345);
-      const year = Math.floor(month / 12);
+      let day = Math.floor(hour / 24);
+      let week = Math.floor(day / 7);
+      let month = Math.floor(week / 4.345);
+      let year = Math.floor(month / 12);
 
-      if (year > 0) {
-         return year === 1 ? "a year ago" : `${year} years ago`;
-      }
-
-      if (month > 0) {
-         return month === 1 ? "a month ago" : `${month} months ago`;
-      }
-
-      if (week > 0) {
-         return week === 1 ? "a week ago" : `${week} weeks ago`;
-      }
-
-      if (day > 0) {
-         return day === 1 ? "a day ago" : `${day} days ago`;
-      }
-
-      if (hour > 0) {
-         return hour === 1 ? "an hour ago" : `${hour} hours ago`;
-      }
-
-      if (minute > 0) {
+      if (year > 0) return year === 1 ? "a year ago" : `${year} years ago`;
+      if (month > 0) return month === 1 ? "a month ago" : `${month} months ago`;
+      if (week > 0) return week === 1 ? "a week ago" : `${week} weeks ago`;
+      if (day > 0) return day === 1 ? "a day ago" : `${day} days ago`;
+      if (hour > 0) return hour === 1 ? "an hour ago" : `${hour} hours ago`;
+      if (minute > 0)
          return minute === 1 ? "a minute ago" : `${minute} minutes ago`;
-      }
 
-      return "a few seconds ago";
+      return Math.floor(difference) > 4
+         ? `${Math.floor(difference)} seconds ago`
+         : "a few seconds ago";
    }
 }
 
-const time = new Time("2020-10-15T15:58:55.480Z");
+const time = new Time("2020-12-11T12:08:22.532Z");
 
 console.log(time.fromNow());
-console.log(time.format("hard"));
-console.log(time.format("medium"));
-console.log(time.format("easy"));
-console.log(time.format("ultimate"));
+console.log(time.getDateFormat("hard", "normal"));
+console.log(time.getDateFormat("medium", "short"));
+console.log(time.getDateFormat("easy", "short"));
